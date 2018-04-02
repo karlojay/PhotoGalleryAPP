a16ae33af02b4a4e3e2e4dda06daa0520704ebe4
karlojay <karlovigilia1234@gmail.com>
2018-04-03 07:25:19 +0800

    PhotoGalleryAPP
    just test

---
+++ PhotoGalleryApp/app/src/main/java/com/example/user/photogalleryapp/ApplicationData.java
@@ -0,0 +1,11 @@
+package com.example.user.photogalleryapp;
+
+/**
+ * Created by user on 01/04/2018.
+ */
+
+public class ApplicationData{
+    public static final String CLIENT_ID = "103babf92fe1445f8bc4a38a8e2cfa58";
+    public static final String CLIENT_SECRET = "a1b7991ed1dc43df9ba8e706d4296b23";
+    public static final String CALLBACK_URL = "instagram://connect";
+}
